5dad5fd65b91b796bb07cb43b3c4f6343123c11b
Lucrecia Martin <82980185+lucreciam@users.noreply.github.com>
2024-10-09 15:07:26 -0300

    Primer modelo de proyecto básico

---
+++ demo/demo/src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+public abstract class Usuario {
+
+    private int id_usuario;
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private String contraseña;
+
+}
---
+++ demo/demo/src/main/java/com/example/demo/model/UsuarioHuesped.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+public class UsuarioHuesped extends Usuario{
+
+    private List<Reserva> reservas =
+
+
+}
